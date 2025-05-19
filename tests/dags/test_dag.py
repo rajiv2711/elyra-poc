1fef2d9e553c764a8b77087b84a09f24eab2275b
Victor Fonseca <victortales@gmail.com>
2025-05-19 22:34:55 +0200

    Add tests/dags directory with test DAG

---
+++ tests/dags/test_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 5, 19),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'test_dag',
+    default_args=default_args,
+    description='A simple test DAG',
+    schedule_interval=None,
+)
+
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
+
+t2 = BashOperator(
+    task_id='sleep',
+    bash_command='sleep 5',
+    dag=dag,
+)
+
+t1 >> t2
