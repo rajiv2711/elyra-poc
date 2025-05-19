427183a1275f74b929878f619e5c1a531bcd74a3
Victor Fonseca <victortales@gmail.com>
2025-05-20 01:55:25 +0200

    Pushed DAG weather-dataprocessing-0519235524

---
+++ weather-dataprocessing-0519235524.py
@@ -0,0 +1,147 @@
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.kubernetes.secret import Secret
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "weather-dataprocessing-0519235524",
+}
+
+dag = DAG(
+    "weather-dataprocessing-0519235524",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.15.0 pipeline editor using `weather-dataprocessing.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: config.py
+
+op_eb249098_94e7_4e62_87f8_63eac26af72b = KubernetesPodOperator(
+    name="config",
+    namespace="default",
+    image="continuumio/anaconda3@sha256:a2816acd3acda208d92e0bf6c11eb41fda9009ea20f24e123dbf84bb4bd4c4b8",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'weather-dataprocessing' --cos-endpoint http://minio.minio-system.svc.cluster.local:9000 --cos-bucket elyra-airflow --cos-directory 'weather-dataprocessing-0519235524' --cos-dependencies-archive 'config-eb249098-94e7-4e62-87f8-63eac26af72b.tar.gz' --file 'config.py' "
+    ],
+    task_id="config",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "weather-dataprocessing-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+
+# Operator source: 01-generate-data-simple.py
+
+op_8dcb9435_7819_4d42_aa04_10c2af047d78 = KubernetesPodOperator(
+    name="01_generate_data_simple",
+    namespace="default",
+    image="continuumio/anaconda3@sha256:a2816acd3acda208d92e0bf6c11eb41fda9009ea20f24e123dbf84bb4bd4c4b8",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'weather-dataprocessing' --cos-endpoint http://minio.minio-system.svc.cluster.local:9000 --cos-bucket elyra-airflow --cos-directory 'weather-dataprocessing-0519235524' --cos-dependencies-archive '01-generate-data-simple-8dcb9435-7819-4d42-aa04-10c2af047d78.tar.gz' --file '01-generate-data-simple.py' "
+    ],
+    task_id="01_generate_data_simple",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "weather-dataprocessing-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+op_8dcb9435_7819_4d42_aa04_10c2af047d78 << op_eb249098_94e7_4e62_87f8_63eac26af72b
+
+
+# Operator source: 02-process-data-simple.py
+
+op_8317bdf0_43a0_41e2_81fe_b0ed4ac1c5cd = KubernetesPodOperator(
+    name="02_process_data_simple",
+    namespace="default",
+    image="continuumio/anaconda3@sha256:a2816acd3acda208d92e0bf6c11eb41fda9009ea20f24e123dbf84bb4bd4c4b8",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'weather-dataprocessing' --cos-endpoint http://minio.minio-system.svc.cluster.local:9000 --cos-bucket elyra-airflow --cos-directory 'weather-dataprocessing-0519235524' --cos-dependencies-archive '02-process-data-simple-8317bdf0-43a0-41e2-81fe-b0ed4ac1c5cd.tar.gz' --file '02-process-data-simple.py' "
+    ],
+    task_id="02_process_data_simple",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "weather-dataprocessing-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+op_8317bdf0_43a0_41e2_81fe_b0ed4ac1c5cd << op_8dcb9435_7819_4d42_aa04_10c2af047d78
+
+
+# Operator source: 03-visualize-data-simple.py
+
+op_70478bc1_e4e1_4227_94f6_19f5bb5af4b4 = KubernetesPodOperator(
+    name="03_visualize_data_simple",
+    namespace="default",
+    image="continuumio/anaconda3@sha256:a2816acd3acda208d92e0bf6c11eb41fda9009ea20f24e123dbf84bb4bd4c4b8",
+    cmds=["sh", "-c"],
+    arguments=[
+        "mkdir -p ./jupyter-work-dir/ && cd ./jupyter-work-dir/ && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/elyra/airflow/bootstrapper.py --output bootstrapper.py && echo 'Downloading https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt' && curl --fail -H 'Cache-Control: no-cache' -L https://raw.githubusercontent.com/elyra-ai/elyra/v3.15.0/etc/generic/requirements-elyra.txt --output requirements-elyra.txt && python3 -m pip install packaging && python3 -m pip freeze > requirements-current.txt && python3 bootstrapper.py --pipeline-name 'weather-dataprocessing' --cos-endpoint http://minio.minio-system.svc.cluster.local:9000 --cos-bucket elyra-airflow --cos-directory 'weather-dataprocessing-0519235524' --cos-dependencies-archive '03-visualize-data-simple-70478bc1-e4e1-4227-94f6-19f5bb5af4b4.tar.gz' --file '03-visualize-data-simple.py' "
+    ],
+    task_id="03_visualize_data_simple",
+    env_vars={
+        "ELYRA_RUNTIME_ENV": "airflow",
+        "AWS_ACCESS_KEY_ID": "minio",
+        "AWS_SECRET_ACCESS_KEY": "minio123",
+        "ELYRA_ENABLE_PIPELINE_INFO": "True",
+        "ELYRA_RUN_NAME": "weather-dataprocessing-{{ ts_nodash }}",
+    },
+    volumes=[],
+    volume_mounts=[],
+    secrets=[],
+    annotations={},
+    labels={},
+    tolerations=[],
+    in_cluster=True,
+    config_file="None",
+    dag=dag,
+)
+
+op_70478bc1_e4e1_4227_94f6_19f5bb5af4b4 << op_8317bdf0_43a0_41e2_81fe_b0ed4ac1c5cd
