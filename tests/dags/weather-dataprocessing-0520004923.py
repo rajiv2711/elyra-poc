f9853989f8f47eb836c5f49699db95615ad9b8c8
Victor Fonseca <victortales@gmail.com>
2025-05-20 02:50:56 +0200

    - New DAG

--- tests/dags/weather-dataprocessing-0520004923.py
+++ tests/dags/weather-dataprocessing-0520004923.py
@@ -1,6 +1,6 @@
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.contrib.kubernetes.volume_mount import VolumeMount
-from airflow.contrib.kubernetes.volume import Volume
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from kubernetes.client.models import V1VolumeMount as VolumeMount
+from kubernetes.client.models import V1Volume as Volume
 from airflow.kubernetes.secret import Secret
 from airflow import DAG
 from airflow.utils.dates import days_ago
